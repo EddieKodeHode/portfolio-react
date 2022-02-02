b251223df9a1f5f8898989662bab3363c6d92bfb
EddieKodeHode <eddie.kodehode@gmail.com>
2022-02-02 09:15:34 +0100

    Added Logo and Nav

---
+++ src/content/Logo.js
@@ -0,0 +1,7 @@
+function Logo() {
+    return (
+        <h1 className='logo'>Hello.</h1>
+    );
+  }
+  
+  export default Logo;
---
+++ src/content/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <nav>
+        <p>H</p>
+        <p>e</p>
+        <p>l</p>
+        <p>l</p>
+        <p>o</p>
+    </nav>
+  );
+}
+
+export default Nav;
